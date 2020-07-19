e1a14055079a9047669207d4167b0a8a5e331860
irgiam <imuqsit@gmail.com>
2020-07-19 21:14:01 +0700

    add pathfinding from brackeys

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 200f;
+    public float nextWaypointDistance = 3f;
+
+    Path path;
+    int currentWayPoint = 0;
+    bool reachEndOfPath = false;
+
+    Seeker seeker;
+    Rigidbody2D rigidBody;
+
+    private void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        InvokeRepeating("UpdatePath", 0f, 0.5f);
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+            seeker.StartPath(rigidBody.position, target.position, OnPathComplete);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (path == null)
+        {
+            return;
+        }
+        if (currentWayPoint >= path.vectorPath.Count)
+        {
+            reachEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reachEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWayPoint] - rigidBody.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+        rigidBody.AddForce(force);
+
+        float distance = Vector2.Distance(rigidBody.position, path.vectorPath[currentWayPoint]);
+
+        if (distance < nextWaypointDistance)
+        {
+            currentWayPoint++;
+        }
+
+        if (rigidBody.velocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (rigidBody.velocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWayPoint = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyFlipPathfinding.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyFlipPathfinding : MonoBehaviour
+{
+    public AIPath thisAiPath;
+    // Update is called once per frame
+    void Update()
+    {
+        if (thisAiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-2.27f, 2.27f, 2.27f);
+        }
+        else if (thisAiPath.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(2.27f, 2.27f, 2.27f);
+        }
+    }
+}
